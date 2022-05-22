d1fcb81eec09631d4b1b01fe0aac8205edaf718c
Aleksandr <aldrobakha@gmail.com>
2022-05-23 00:40:39 +0300

    add HW5

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -7,7 +7,7 @@ PrintArray(FillArray(array.GetLength(0), array.GetLength(1), array.GetLength(2))
 
 int[,,] FillArray(int i, int j, int k)
 {
-    int[,,] array = new int[i, j, k];
+    int[,,] collection = new int[i, j, k];
     int minNumber = 0;
     int number = 10;
     int[] temp = new int[89];
@@ -16,16 +16,16 @@ int[,,] FillArray(int i, int j, int k)
     {    
         temp[x] = number++;
     }
-    for (int l = 0; l < array.GetLength(0); l++)
-        for (int m = 0; m < array.GetLength(1); m++)
-            for (int n = 0; n < array.GetLength(2); n++)
+    for (int l = 0; l < collection.GetLength(0); l++)
+        for (int m = 0; m < collection.GetLength(1); m++)
+            for (int n = 0; n < collection.GetLength(2); n++)
             {
                 int rndNumber = new Random().Next(minNumber, temp.Length);
-                array[l, m, n] = temp[rndNumber];
+                collection[l, m, n] = temp[rndNumber];
                 temp[rndNumber] = temp[minNumber];
                 minNumber++;
             }
-    return array;
+    return collection;
 }
 
 void PrintArray(int[,,] inputArray)
