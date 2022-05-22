0a3eccc514ded40997280a24d87d789749228452
Aleksandr <aldrobakha@gmail.com>
2022-05-23 00:36:45 +0300

    add HW5

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -1,45 +1,31 @@
 ﻿/* Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента. */
 
-
 int[,,] array = new int[3, 3, 3];
 
-FillArray(array);
-PrintArray(array);
+PrintArray(CreateArray(array.GetLength(0), array.GetLength(1), array.GetLength(2)));
 
-void FillArray(int[,,] collection)
+int[,,] CreateArray(int i, int j, int k)
 {
-    for(int i = 0; i < collection.GetLength(0); i++)
-    {
-        for(int j = 0; j < collection.GetLength(1); j++)
-        {
-            for (int k = 0; k < collection.GetLength(2); k++)
-            {
-                bool found = false;
-                int temp;
-                while (!found)
-                {
-                    found = false;
-                    temp = new Random().Next(10, 100);
-                    for(int l = 0; l < collection.GetLength(0); l++)
-                    {
-                        for(int m = 0; m < collection.GetLength(1); m++)
-                        {
-                            for (int n = 0; n < collection.GetLength(2); n++)
-                            {
-                                if (temp == collection[l, m, n])
-                                {
-                                    found = true;
-                                }
-                                collection[i, j, k] = temp;  
-                            }
-                        }
-                    }         
-                }
-                   
-            }
-        }
+    int[,,] array = new int[i, j, k];
+    int minNumber = 0;
+    int number = 10;
+    int[] temp = new int[89];
+
+    for (int x = 0; x < temp.Length; x++)
+    {    
+        temp[x] = number++;
     }
+    for (int l = 0; l < array.GetLength(0); l++)
+        for (int m = 0; m < array.GetLength(1); m++)
+            for (int n = 0; n < array.GetLength(2); n++)
+            {
+                int rndNumber = new Random().Next(minNumber, temp.Length);
+                array[l, m, n] = temp[rndNumber];
+                temp[rndNumber] = temp[minNumber];
+                minNumber++;
+            }
+    return array;
 }
 
 void PrintArray(int[,,] inputArray)
@@ -57,6 +43,3 @@ void PrintArray(int[,,] inputArray)
         Console.WriteLine();
     }
 }
-
-
-
